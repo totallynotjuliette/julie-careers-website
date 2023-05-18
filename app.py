0731d61c107c4d8bbf31e3b8d79cf02f085a5726
Juliette Garcia <juliegarcia084@gmail.com>
2023-05-17 20:05:27 -0400

    basic bootstrap

--- app.py
+++ app.py
@@ -2,9 +2,36 @@ from flask import Flask, render_template
 
 app = Flask(__name__) # app is object of the class "Flask"
 
+JOBS = [
+    {
+        "id": 1,
+        "title": "Data Analyst",
+        "location": "Miami, Florida",
+        "salary": "$85,000"
+    },
+    {
+        "id": 2,
+        "title": "Data Scientist",
+        "location": "Tampa, Florida", 
+    },
+    {
+        "id": 3,
+        "title": "Frontend Engineer",
+        "location": "Los Angeles, California",
+        "salary": "$63,000"
+    },
+    {
+        "id": 4,
+        "title": "Backend Engineer",
+        "location": "Boston, Massachusetts",
+        "salary": "$70,000"
+    },
+]
+
+
 @app.route("/") # empty route / homepage
 def hello(): # when this url is accessed...
-    return render_template("home.html")
+    return render_template("home.html", jobs=JOBS)
 
 if __name__ == "__main__": # this runs w app.py while the routes dont
     app.run(host="0.0.0.0", debug=True) 
